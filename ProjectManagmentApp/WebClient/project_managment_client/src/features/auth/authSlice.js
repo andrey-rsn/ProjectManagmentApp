7906328b61e305cd7b3aba799c73429dbfd09b0f
andrey-rsn <korowai98.ag@gmail.com>
2023-01-12 20:54:59 +0300

    implement cookies auth

--- ProjectManagmentApp/WebClient/project_managment_client/src/features/auth/authSlice.js
+++ ProjectManagmentApp/WebClient/project_managment_client/src/features/auth/authSlice.js
@@ -1,18 +1,26 @@
 import { createSlice } from "@reduxjs/toolkit";
+import Cookies from 'universal-cookie';
+
+const cookies = new Cookies();
 
 const authSlice = createSlice({
     name: 'auth',
     initialState: {user: null, token: null},
     reducers: {
         setCredentials: (state, action) => {
-            console.log(action);
-            const { user_name, access_token } = action.payload;
-            state.user = user_name;
-            state.token = access_token;
+            const { user_name, access_token,user_Id } = action.payload;
+
+            let expires = new Date();
+            expires.setTime(expires.getTime() + (15 * 1000));
+
+            cookies.set('access_token', access_token, expires);
+            cookies.set('user_id', user_Id, expires);
+            cookies.set('user_name', user_name, expires);
         },
-        logOut: (state, action) => {
-            state.user = null;
-            state.token = null;
+        logOut: () => {
+            cookies.remove('access_token');
+            cookies.remove('user_id');
+            cookies.remove('user_name');
         }
     }
 })
@@ -21,5 +29,5 @@ export const {setCredentials, logOut} = authSlice.actions;
 
 export default authSlice.reducer;
 
-export const selectCurrentUser = (state) => state.auth.user;
-export const selectCurrentToken = (state) => state.auth.token;
+export const selectCurrentUser = (state) => cookies.get('user_name');
+export const selectCurrentToken = (state) => cookies.get('access_token');
