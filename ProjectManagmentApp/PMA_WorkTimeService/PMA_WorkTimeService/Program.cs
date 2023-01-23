0c7d05b40d61968e75af74f7eb6ef90551c76a23
andrey-rsn <korowai98.ag@gmail.com>
2023-01-23 16:58:59 +0300

    create authorization middleware

--- ProjectManagmentApp/PMA_WorkTimeService/PMA_WorkTimeService/Program.cs
+++ ProjectManagmentApp/PMA_WorkTimeService/PMA_WorkTimeService/Program.cs
@@ -1,11 +1,18 @@
+using PMA_WorkTimeService.Middleware;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient("authClient", c =>
+{
+    c.BaseAddress = new Uri("http://localhost:5069");
+});
+builder.Services.AddCors();
+
 
 var app = builder.Build();
 
@@ -16,10 +23,14 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.UseAuthorizationMiddleware();
 
 app.Run();
