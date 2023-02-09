9e5c95e3efc2599afbf00d5a36a4094cd6c4f1ca
andrey-rsn <korowai98.ag@gmail.com>
2023-02-09 20:24:26 +0300

    Added auth service and db seed in user task service

--- ProjectManagmentApp/PMA_TasksService/PMA_TasksService/Program.cs
+++ ProjectManagmentApp/PMA_TasksService/PMA_TasksService/Program.cs
@@ -1,9 +1,28 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.EntityFrameworkCore;
+using PMA_TasksService.Data;
+using PMA_TasksService.Extensions;
+using PMA_TasksService.Middleware;
+using PMA_TasksService.Repositories;
+using PMA_TasksService.Repositories.Interfaces;
 
+var builder = WebApplication.CreateBuilder(args);
+var Configuration = builder.Configuration;
 
 builder.Services.AddControllers();
+builder.Services.AddAutoMapper(typeof(Program));
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(Configuration.GetConnectionString("TaskServiceDatabase")));
+builder.Services.AddScoped<IUserTaskRepository, UserTaskRepository>();
+builder.Services.AddScoped<ICommentRepository, CommentRepository>();
+builder.Services.AddScoped<ITaskCommentsRepository, TaskCommentsRepository>();
+builder.Services.AddScoped<IUserTaskStatusRepository, UserTaskStatusRepository>();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient("authClient", c =>
+{
+    c.BaseAddress = new Uri(Configuration.GetConnectionString("IdentityService"));
+});
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -14,8 +33,18 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+app.UseAuthorizationMiddleware();
 
 app.MapControllers();
+
+app.MigrateDatabase<ApplicationDbContext>((context, service) =>
+{
+    var logger = app.Services.GetService<ILogger<ApplicationDbContext>>();
+    DatabaseSeed
+                .SeedAsync(context, logger)
+                .Wait();
+});
 
 app.Run();
