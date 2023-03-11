e803c28489523603fa968df45f3e51cf13a230af
andrey-rsn <korowai98.ag@gmail.com>
2023-03-11 22:08:35 +0300

    Add Database DI

--- ProjectManagmentApp/PMA_ProjectsService/PMA_ProjectsService/Program.cs
+++ ProjectManagmentApp/PMA_ProjectsService/PMA_ProjectsService/Program.cs
@@ -1,13 +1,16 @@
+using Microsoft.EntityFrameworkCore;
 using PMA_ProjectsService.Data;
 using PMA_ProjectsService.Extensions;
 using PMA_ProjectsService.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
+var Configuration = builder.Configuration;
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddDbContext<ApplicationDbContext>(options =>
+    options.UseNpgsql(Configuration.GetConnectionString("ProjectsServiceDatabase")));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
