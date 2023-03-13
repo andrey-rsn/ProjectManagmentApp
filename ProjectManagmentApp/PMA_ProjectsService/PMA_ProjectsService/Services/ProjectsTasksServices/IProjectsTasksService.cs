308d9fb3deb9d16ae8e774a1fcb358a79481274f
andrey-rsn <korowai98.ag@gmail.com>
2023-03-13 15:21:49 +0300

    create projectsTasks service

---
+++ ProjectManagmentApp/PMA_ProjectsService/PMA_ProjectsService/Services/ProjectsTasksServices/IProjectsTasksService.cs
@@ -0,0 +1,13 @@
+﻿using PMA_ProjectsService.Models.DTOs;
+
+namespace PMA_ProjectsService.Services.ProjectsTasksServices
+{
+    public interface IProjectsTasksService
+    {
+        Task<IEnumerable<ProjectsTasksDTO>> GetByTaskId(int TaskId);
+        Task<IEnumerable<ProjectsTasksDTO>> GetByTaskAndProjectId(int TaskId, int ProjectId);
+        Task<IEnumerable<ProjectsTasksDTO>> GetByProjectId(int ProjectId);
+        Task<bool> DeleteById(int Id);
+
+    }
+}
---
+++ ProjectManagmentApp/PMA_ProjectsService/PMA_ProjectsService/Services/ProjectsTasksServices/ProjectsTasksService.cs
@@ -0,0 +1,52 @@
+﻿using PMA_ProjectsService.Models;
+using PMA_ProjectsService.Models.DTOs;
+using PMA_ProjectsService.Repositories.Interfaces;
+
+namespace PMA_ProjectsService.Services.ProjectsTasksServices
+{
+    public class ProjectsTasksService : IProjectsTasksService
+    {
+        private readonly IProjectsTasksRepository _projectsTasksRepository;
+
+        public ProjectsTasksService(IProjectsTasksRepository projectsTasksRepository)
+        {
+            _projectsTasksRepository = projectsTasksRepository;
+        }
+
+        public async Task<bool> DeleteById(int Id)
+        {
+            var project = await _projectsTasksRepository.GetByIdAsync(Id);
+
+            if(project != null) 
+            {
+                await _projectsTasksRepository.DeleteAsync(project);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+
+        public async Task<IEnumerable<ProjectsTasksDTO>> GetByProjectId(int ProjectId)
+        {
+            var Projects = await _projectsTasksRepository.GetAsync(x => x.ProjectId == ProjectId);
+
+            return Projects;
+        }
+
+        public async Task<IEnumerable<ProjectsTasksDTO>> GetByTaskAndProjectId(int TaskId, int ProjectId)
+        {
+            var Projects = await _projectsTasksRepository.GetAsync(x => x.ProjectId == ProjectId && x.TaskId == TaskId);
+
+            return Projects;
+        }
+
+        public async Task<IEnumerable<ProjectsTasksDTO>> GetByTaskId(int TaskId)
+        {
+            var Projects = await _projectsTasksRepository.GetAsync(x => x.TaskId == TaskId);
+
+            return Projects;
+        }
+    }
+}
