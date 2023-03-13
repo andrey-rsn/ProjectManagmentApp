0e24fceaca0ecdffbee941469e597503ba26ab7f
andrey-rsn <korowai98.ag@gmail.com>
2023-03-13 13:27:21 +0300

    Add EmployeesAttachedToProjectsService

---
+++ ProjectManagmentApp/PMA_ProjectsService/PMA_ProjectsService/Services/EmployeesAttachedToProjectsServices/IEmployeesAttachedToProjectsService.cs
@@ -0,0 +1,12 @@
+﻿using PMA_ProjectsService.Models.DTOs;
+
+namespace PMA_ProjectsService.Services.EmployeesAttachedToProjectsServices
+{
+    public interface IEmployeesAttachedToProjectsService
+    {
+        Task<IEnumerable<EmployeesAttachedToProjectsDTO>> GetByUserId(int UserId);
+        Task<IEnumerable<EmployeesAttachedToProjectsDTO>> GetByProjectId(int ProjectId);
+        Task<EmployeesAttachedToProjectsDTO> Add(EmployeesAttachedToProjectsDTO EmployeesAttachedToProjects);
+        Task<bool> DeleteByUserAndProjectId(int UserId, int ProjectId);
+    }
+}
