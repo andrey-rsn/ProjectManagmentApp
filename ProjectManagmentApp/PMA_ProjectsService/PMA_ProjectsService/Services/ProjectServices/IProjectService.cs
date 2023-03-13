96de1e937c5138e1e09775aa72f57da3aeeb721d
andrey-rsn <korowai98.ag@gmail.com>
2023-03-13 13:06:00 +0300

    create project service

---
+++ ProjectManagmentApp/PMA_ProjectsService/PMA_ProjectsService/Services/ProjectServices/IProjectService.cs
@@ -0,0 +1,12 @@
+﻿using PMA_ProjectsService.Models.DTOs;
+
+namespace PMA_ProjectsService.Services.ProjectServices
+{
+    public interface IProjectService
+    {
+        Task<IEnumerable<ProjectDTO>> GetAll(int limit = 10);
+        Task<ProjectDTO> GetById(int id);
+        Task<ProjectDTO> Update(ProjectDTO projectDTO);
+        Task<bool> DeleteById(int id);
+    }
+}
---
+++ ProjectManagmentApp/PMA_ProjectsService/PMA_ProjectsService/Services/ProjectServices/ProjectService.cs
@@ -0,0 +1,53 @@
+﻿using AutoMapper;
+using PMA_ProjectsService.Models.DTOs;
+using PMA_ProjectsService.Repositories.Interfaces;
+
+namespace PMA_ProjectsService.Services.ProjectServices
+{
+    public class ProjectService : IProjectService
+    {
+        private readonly IProjectRepository _projectRepository;
+
+        public ProjectService(IProjectRepository projectRepository)
+        {
+            _projectRepository = projectRepository;
+        }
+
+        public async Task<bool> DeleteById(int id)
+        {
+            var project = await _projectRepository.GetByIdAsync(id);
+
+            if(project != null) 
+            {
+                var result = await _projectRepository.DeleteAsync(project);
+                return result != null;
+            }
+            else
+            {
+                return false;
+            }
+                
+        }
+
+        public async Task<IEnumerable<ProjectDTO>> GetAll(int limit = 10)
+        {
+            var projects = await _projectRepository.GetAllAsync(limit);
+
+            return projects;
+        }
+
+        public async Task<ProjectDTO> GetById(int id)
+        {
+            var project = await _projectRepository.GetByIdAsync(id);
+
+            return project;
+        }
+
+        public async Task<ProjectDTO> Update(ProjectDTO projectDTO)
+        {
+            var result = await _projectRepository.UpdateAsync(projectDTO);
+
+            return result;
+        }
+    }
+}
