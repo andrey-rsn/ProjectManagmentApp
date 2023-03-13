3e78f4c2ca55b4c4a4ddbaf66f1e3a713630c269
andrey-rsn <korowai98.ag@gmail.com>
2023-03-13 15:06:04 +0300

    Add ProjectsTasksRepository

---
+++ ProjectManagmentApp/PMA_ProjectsService/PMA_ProjectsService/Repositories/Interfaces/IProjectsTasksRepository.cs
@@ -0,0 +1,9 @@
+﻿using PMA_ProjectsService.Models;
+using PMA_ProjectsService.Models.DTOs;
+
+namespace PMA_ProjectsService.Repositories.Interfaces
+{
+    public interface IProjectsTasksRepository : IBaseAsyncRepository<ProjectsTasksDTO,ProjectsTasksModel>
+    {
+    }
+}
---
+++ ProjectManagmentApp/PMA_ProjectsService/PMA_ProjectsService/Repositories/ProjectsTasksRepository.cs
@@ -0,0 +1,15 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PMA_ProjectsService.Models;
+using PMA_ProjectsService.Models.DTOs;
+using PMA_ProjectsService.Repositories.Interfaces;
+
+namespace PMA_ProjectsService.Repositories
+{
+    public class ProjectsTasksRepository : BaseAsyncRepository<ProjectsTasksDTO, ProjectsTasksModel>, IProjectsTasksRepository
+    {
+        public ProjectsTasksRepository(DbContext dbContext, IMapper mapper) : base(dbContext, mapper)
+        {
+        }
+    }
+}
