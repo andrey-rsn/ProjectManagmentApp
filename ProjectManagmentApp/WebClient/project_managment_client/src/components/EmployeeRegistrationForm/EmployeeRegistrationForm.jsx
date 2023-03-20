0678fe6a2b3331b9756876ce75530ddb31c088f9
andrey-rsn <korowai98.ag@gmail.com>
2023-03-20 20:57:22 +0300

    create layout of employeeRegistration page and form

---
+++ ProjectManagmentApp/WebClient/project_managment_client/src/components/EmployeeRegistrationForm/EmployeeRegistrationForm.jsx
@@ -0,0 +1,237 @@
+import "./EmployeeRegistrationForm.css";
+import TextField from '@mui/material/TextField';
+import * as yup from 'yup';
+import { useFormik } from 'formik';
+import { useMemo, useState } from "react";
+import MenuItem from '@mui/material/MenuItem';
+import FormControl from '@mui/material/FormControl';
+import Select from '@mui/material/Select';
+import InputLabel from '@mui/material/InputLabel';
+import Divider from '@mui/material/Divider';
+import Button from '@mui/material/Button';
+
+const EmployeeRegistrationForm = () => {
+
+    const [registrationError, setRegistrationError] = useState();
+
+    const [userInfo, setUserInfo] = useState({
+        email:'',
+        login:'',
+        password:'',
+        firstName:'',
+        secondName:'',
+        patronymic:'',
+        role:'',
+        positionId: 0
+    });
+
+    const validationSchema = yup.object({
+        email: yup
+            .string('Введите Email')
+            .email('Некорректное значение Email')
+            .required('Поле обязательно для заполенения'),
+        login: yup
+            .string('Введите логин')
+            .required('Поле обязательно для заполенения'),
+        password: yup
+            .string('Введите пароль')
+            .required('Поле обязательно для заполенения'),
+        firstName: yup
+            .string('Введите имя')
+            .required('Поле обязательно для заполенения'),
+        secondName: yup
+            .string('Введите фамилию')
+            .required('Поле обязательно для заполенения'),
+        patronymic: yup
+            .string('Введите отчество')
+    });
+
+    const formik = useFormik({
+        initialValues: {
+            email:'',
+            login:'',
+            password:'',
+            firstName:'',
+            secondName:'',
+            patronymic:'',
+            role:'',
+            positionId: 0
+        },
+        validationSchema: validationSchema,
+        onSubmit: (values) => {
+            onFormSubmit(values);
+        },
+    });
+
+
+    const onFormSubmit = async (values) => {
+
+    }
+
+    return (
+        <form className="employee-registration-form" onSubmit={formik.handleSubmit}>
+            <div className="employee-registration-form__header-text">
+                <p>Регистрация сотрудника</p>
+            </div>
+            <Divider sx={{ backgroundColor: 'grey' }} />
+            <div className="employee-registration-form__registration-form-body registration-form-body">
+                <div className="registration-form-body__input-element input-element">
+                    <div className="input-element__element-text">
+                        <p>Email</p>
+                    </div>
+                    <div className="input-element__input-box">
+                        <TextField
+                            fullWidth
+                            id="email"
+                            name="email"
+                            label="Email"
+                            value={formik.values.email}
+                            onChange={formik.handleChange}
+                            error={formik.touched.email && Boolean(formik.errors.email) || Boolean(registrationError)}
+                            helperText={formik.touched.email && formik.errors.email}
+                            sx={{ height: '100% !important' }}
+                        />
+                    </div>
+                </div>
+                <div className="registration-form-body__input-element input-element">
+                    <div className="input-element__element-text">
+                        <p>Логин</p>
+                    </div>
+                    <div className="input-element__input-box">
+                        <TextField
+                            fullWidth
+                            id="login"
+                            name="login"
+                            label="Логин"
+                            value={formik.values.login}
+                            onChange={formik.handleChange}
+                            error={formik.touched.login && Boolean(formik.errors.login) || Boolean(registrationError)}
+                            helperText={formik.touched.login && formik.errors.login}
+                            sx={{ height: '100% !important' }}
+                        />
+                    </div>
+                </div>
+                <div className="registration-form-body__input-element input-element">
+                    <div className="input-element__element-text">
+                        <p>Пароль</p>
+                    </div>
+                    <div className="input-element__input-box">
+                        <TextField
+                            fullWidth
+                            id="password"
+                            name="password"
+                            label="Пароль"
+                            value={formik.values.password}
+                            onChange={formik.handleChange}
+                            error={formik.touched.password && Boolean(formik.errors.password) || Boolean(registrationError)}
+                            helperText={formik.touched.password && formik.errors.password}
+                            sx={{ height: '100% !important' }}
+                        />
+                    </div>
+                </div>
+                <div className="registration-form-body__input-element input-element">
+                    <div className="input-element__element-text">
+                        <p>Имя</p>
+                    </div>
+                    <div className="input-element__input-box">
+                        <TextField
+                            fullWidth
+                            id="firstName"
+                            name="firstName"
+                            label="Имя"
+                            value={formik.values.firstName}
+                            onChange={formik.handleChange}
+                            error={formik.touched.firstName && Boolean(formik.errors.firstName) || Boolean(registrationError)}
+                            helperText={formik.touched.firstName && formik.errors.firstName}
+                            sx={{ height: '100% !important' }}
+                        />
+                    </div>
+                </div>
+                <div className="registration-form-body__input-element input-element">
+                    <div className="input-element__element-text">
+                        <p>Фамилия</p>
+                    </div>
+                    <div className="input-element__input-box">
+                        <TextField
+                            fullWidth
+                            id="secondName"
+                            name="secondName"
+                            label="Фамилия"
+                            value={formik.values.secondName}
+                            onChange={formik.handleChange}
+                            error={formik.touched.secondName && Boolean(formik.errors.secondName) || Boolean(registrationError)}
+                            helperText={formik.touched.secondName && formik.errors.secondName}
+                            sx={{ height: '100% !important' }}
+                        />
+                    </div>
+                </div>
+                <div className="registration-form-body__input-element input-element">
+                    <div className="input-element__element-text">
+                        <p>Отчество</p>
+                    </div>
+                    <div className="input-element__input-box">
+                        <TextField
+                            fullWidth
+                            id="patronymic"
+                            name="patronymic"
+                            label="Отчество"
+                            value={formik.values.patronymic}
+                            onChange={formik.handleChange}
+                            error={formik.touched.patronymic && Boolean(formik.errors.patronymic) || Boolean(registrationError)}
+                            helperText={formik.touched.patronymic && formik.errors.patronymic}
+                            sx={{ height: '100% !important' }}
+                        />
+                    </div>
+                </div>
+                <div className="registration-form-body__input-element input-element">
+                    <div className="input-element__element-text">
+                        <p>Роль</p>
+                    </div>
+                    <div className="input-element__input-box">
+                        <FormControl sx={{ m: 1, width:'100%', textAlign:'start', margin:'0' }} size="small">
+                            <InputLabel id="demo-select-small"></InputLabel>
+                            <Select
+                                id="role"
+                                name="role"
+                                value={formik.values.role}
+                                onChange={formik.handleChange}
+                            >
+                                <MenuItem value={'PM'}>Проектный менеджер</MenuItem>
+                                <MenuItem value={'Employee'}>Сотрудник</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </div>
+                </div>
+                <div className="registration-form-body__input-element input-element">
+                    <div className="input-element__element-text">
+                        <p>Должность</p>
+                    </div>
+                    <div className="input-element__input-box">
+                        <FormControl sx={{ m: 1, width:'100%', textAlign:'start', margin:'0' }} size="small">
+                            <InputLabel id="demo-select-small"></InputLabel>
+                            <Select
+                                id="positionId"
+                                name="positionId"
+                                value={formik.values.statusId}
+                                onChange={formik.handleChange}
+                            >
+                                <MenuItem value={1}>Проектный менеджер</MenuItem>
+                                <MenuItem value={2}>Сотрудник</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </div>
+                </div>
+            </div>
+            <Divider sx={{ backgroundColor: 'grey' }} />
+            <div className="employee-registration-form__form-bottom form-bottom">
+                <div className="form-bottom__actions">
+                    <Button variant="contained" type="submit">
+                        Зарегистрировать сотрудника
+                    </Button>
+                </div>
+            </div>
+        </form>
+    )
+}
+
+export default EmployeeRegistrationForm;
--- ProjectManagmentApp/WebClient/project_managment_client/src/pages/StartPage/StartPage.jsx
+++ ProjectManagmentApp/WebClient/project_managment_client/src/pages/StartPage/StartPage.jsx
@@ -3,6 +3,7 @@ import MainInfoForm from '../../components/MainInfoForm/MainInfoForm';
 import './StartPage.css';
 import { BrowserRouter as Router, Route, Routes, useParams } from 'react-router-dom';
 import WorkTimePage from '../WorkTimePage/WorkTimePage';
+import EmployeeRegistrationPage from '../EmployeeRegistrationPage/EmployeeRegistrationPage';
 
 const StartPage = () => {
 
@@ -15,7 +16,8 @@ const StartPage = () => {
             <div className="start-page__content">
                 <Routes>
                     <Route path="/" element={<MainInfoForm/>} />
-                    <Route path="/workTime" element={<WorkTimePage/>} /> 
+                    <Route path="/workTime" element={<WorkTimePage/>} />
+                    <Route path="/addEmployee" element={<EmployeeRegistrationPage/>} />
                 </Routes>
                 
             </div>
