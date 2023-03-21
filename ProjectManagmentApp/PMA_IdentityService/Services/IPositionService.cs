f9ff417d185046cc3becbf1782ed37c003ed8f89
andrey-rsn <korowai98.ag@gmail.com>
2023-03-21 12:30:00 +0300

    cerate positions service and controller

---
+++ ProjectManagmentApp/PMA_IdentityService/Services/IPositionService.cs
@@ -0,0 +1,9 @@
+﻿using PMA_IdentityService.Models.DTOs;
+
+namespace PMA_IdentityService.Services
+{
+    public interface IPositionService
+    {
+        Task<IEnumerable<PositionDTO>> GetAll();
+    }
+}
---
+++ ProjectManagmentApp/PMA_IdentityService/Services/PositionService.cs
@@ -0,0 +1,20 @@
+﻿using PMA_IdentityService.Models.DTOs;
+using PMA_IdentityService.Repositories;
+
+namespace PMA_IdentityService.Services
+{
+    public class PositionService : IPositionService
+    {
+        private readonly IPositionRepository _positionRepository;
+
+        public PositionService(IPositionRepository positionRepository)
+        {
+            _positionRepository = positionRepository;
+        }
+
+        public async Task<IEnumerable<PositionDTO>> GetAll()
+        {
+            return await _positionRepository.GetAll();
+        }
+    }
+}
