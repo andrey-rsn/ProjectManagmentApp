f37ab08752c369b56e376eb27ff4e875192a46fd
andrey-rsn <korowai98.ag@gmail.com>
2023-03-30 21:43:28 +0300

    add base api settings

--- ProjectManagmentApp/PMA_DocumentationService/PMA_DocumentationService/Program.cs
+++ ProjectManagmentApp/PMA_DocumentationService/PMA_DocumentationService/Program.cs
@@ -1,11 +1,19 @@
-var builder = WebApplication.CreateBuilder(args);
+using Microsoft.Extensions.Configuration;
+using PMA_DocumentationService.Middleware;
 
+var builder = WebApplication.CreateBuilder(args);
+var Configuration = builder.Configuration;
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddHttpClient("authClient", c =>
+{
+    c.BaseAddress = new Uri(Configuration.GetConnectionString("IdentityService"));
+});
+builder.Services.AddCors();
 
 var app = builder.Build();
 
@@ -16,7 +24,9 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
-app.UseAuthorization();
+app.UseCors(builder => builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+
+app.UseAuthorizationMiddleware();
 
 app.MapControllers();
 
