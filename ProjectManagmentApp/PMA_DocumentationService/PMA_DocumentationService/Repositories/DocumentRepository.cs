32e192603c98aab8fef76be06c0340eb293ce8d0
andrey-rsn <korowai98.ag@gmail.com>
2023-03-30 22:20:40 +0300

    Create base repository and document repository

---
+++ ProjectManagmentApp/PMA_DocumentationService/PMA_DocumentationService/Repositories/DocumentRepository.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using Microsoft.EntityFrameworkCore;
+using PMA_DocumentationService.Data;
+using PMA_DocumentationService.Models;
+using PMA_DocumentationService.Models.DTOs;
+using PMA_DocumentationService.Repositories.Interfaces;
+
+namespace PMA_DocumentationService.Repositories
+{
+    public class DocumentRepository : BaseAsyncRepository<DocumentDTO, Document>, IDocumentRepository
+    {
+        public DocumentRepository(ApplicationDbContext dbContext, IMapper mapper) : base(dbContext, mapper)
+        {
+        }
+    }
+}
