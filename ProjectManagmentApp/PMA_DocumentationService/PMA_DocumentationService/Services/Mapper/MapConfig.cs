37a07a96904e5720b95730900180d7ac6b771621
andrey-rsn <korowai98.ag@gmail.com>
2023-03-30 22:01:39 +0300

    add mapper config

--- ProjectManagmentApp/PMA_DocumentationService/PMA_DocumentationService/Services/Mapper/MapConfig.cs
+++ ProjectManagmentApp/PMA_DocumentationService/PMA_DocumentationService/Services/Mapper/MapConfig.cs
@@ -1,6 +1,14 @@
-﻿namespace PMA_DocumentationService.Services.Mapper
+﻿using AutoMapper;
+using PMA_DocumentationService.Models;
+using PMA_DocumentationService.Models.DTOs;
+
+namespace PMA_DocumentationService.Services.Mapper
 {
-    public class MapConfig
+    public class MapConfig:Profile
     {
+        public MapConfig()
+        {
+            CreateMap<Document, DocumentDTO>().ReverseMap();
+        }
     }
 }
