c43436012e2a9197c65d2a086224b54ba1f6aac5
andrey-rsn <korowai98.ag@gmail.com>
2023-03-30 21:54:52 +0300

    fix document model

--- ProjectManagmentApp/PMA_DocumentationService/PMA_DocumentationService/Models/DTOs/DocumentDTO.cs
+++ ProjectManagmentApp/PMA_DocumentationService/PMA_DocumentationService/Models/DTOs/DocumentDTO.cs
@@ -1,6 +1,19 @@
-﻿namespace PMA_DocumentationService.Models.DTOs
+﻿
+namespace PMA_DocumentationService.Models.DTOs
 {
     public class DocumentDTO
     {
+        public int Id { get; set; }
+
+        public string DocumentName { get; set; }
+
+        public string DocumentDescription { get; set; }
+
+        public string FileName { get; set; }
+
+        public string FileUrl { get; set; }
+
+        public DateTime UploadDate { get; set; }
+        public int ProjectId { get; set; }
     }
 }
--- ProjectManagmentApp/PMA_DocumentationService/PMA_DocumentationService/Models/Document.cs
+++ ProjectManagmentApp/PMA_DocumentationService/PMA_DocumentationService/Models/Document.cs
@@ -1,6 +1,29 @@
-﻿namespace PMA_DocumentationService.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace PMA_DocumentationService.Models
 {
     public class Document
     {
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        public string DocumentName { get; set; }
+
+        [Required]
+        public string DocumentDescription { get; set; }
+
+        [Required]
+        public string FileName { get; set; }
+
+        [Required]
+        public string FileUrl { get; set; }
+
+        [Required]
+        public DateTime UploadDate { get; set; }
+
+        [Required]
+        public int ProjectId { get; set; }
+
     }
 }
