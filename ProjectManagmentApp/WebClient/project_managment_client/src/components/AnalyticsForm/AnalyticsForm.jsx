aa5458ad5af6f83b88ea8f1a941c8ce2fc4270d2
andrey-rsn <korowai98.ag@gmail.com>
2023-04-04 22:46:32 +0300

    add tasks analytics loading

--- ProjectManagmentApp/WebClient/project_managment_client/src/components/AnalyticsForm/AnalyticsForm.jsx
+++ ProjectManagmentApp/WebClient/project_managment_client/src/components/AnalyticsForm/AnalyticsForm.jsx
@@ -2,35 +2,55 @@ import "./AnalyticsForm.css";
 import React from 'react';
 import { Chart as ChartJS, ArcElement, Tooltip, Legend } from 'chart.js';
 import { Pie } from 'react-chartjs-2';
+import { useState } from "react";
+import { useLazyGetTasksAnalyticsByProjectQuery } from "../../features/analytics/analyticsApiSlice";
+import { useParams } from "react-router-dom";
+import { useEffect } from "react";
+
 
 ChartJS.register(ArcElement, Tooltip, Legend);
 
-const data = {
-    labels: ['Новые', 'В работе', 'Выполненные', 'Завершённые'],
-    datasets: [
-        {
-            label: 'кол-во задач',
-            data: [12, 19, 3, 5],
-            backgroundColor: [
-                'rgb(125, 125, 125, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)'
-            ],
-            borderColor: [
-                'rgb(125, 125, 125, 1)',
-                'rgba(54, 162, 235, 1)',
-                'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)'
-            ],
-            borderWidth: 1,
-        },
-    ],
-};
+
 
 
 const AnalyticsForm = () => {
+    const [tasksAnalytics, setTasksAnalytics] = useState([1, 0, 0, 0]);
+    const { projectId } = useParams();
+    const [fetchTasksAnalytics, { isLoading: isTasksAnalyticsLoading, isSuccess: isTasksAnalyticsLoaded }] = useLazyGetTasksAnalyticsByProjectQuery();
 
+    useEffect(() => {
+        const load = async () => {
+            await loadData();
+        }
+        load();
+    }, [])
+
+    const loadData = async () => {
+        await fetchTasksAnalytics(projectId).unwrap().then(data => setTasksAnalytics(data)).catch(err => console.log(err));
+    }
+
+    const data = {
+        labels: ['Новые', 'В работе', 'Выполненные', 'Завершённые'],
+        datasets: [
+            {
+                label: 'кол-во задач',
+                data: tasksAnalytics,
+                backgroundColor: [
+                    'rgb(125, 125, 125, 0.2)',
+                    'rgba(54, 162, 235, 0.2)',
+                    'rgba(255, 206, 86, 0.2)',
+                    'rgba(75, 192, 192, 0.2)'
+                ],
+                borderColor: [
+                    'rgb(125, 125, 125, 1)',
+                    'rgba(54, 162, 235, 1)',
+                    'rgba(255, 206, 86, 1)',
+                    'rgba(75, 192, 192, 1)'
+                ],
+                borderWidth: 1,
+            },
+        ],
+    };
 
     return (
         <div className="analytics-form">
@@ -39,7 +59,7 @@ const AnalyticsForm = () => {
             </div>
             <div className="analytics-form__form-body form-body">
                 <div className="form-body__diagram">
-                    <Pie data={data} style={{marginBottom:'20px'}}/>
+                    {isTasksAnalyticsLoading || !isTasksAnalyticsLoaded ? <p>Данные отсутсвуют</p> : <Pie data={data} style={{ marginBottom: '20px' }} />}
                     Статусы задач проекта
                 </div>
             </div>
