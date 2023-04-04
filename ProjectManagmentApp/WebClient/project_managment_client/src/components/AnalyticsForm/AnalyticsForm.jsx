a12afc1fc89bd5c33ab92a1d10b865988a420835
andrey-rsn <korowai98.ag@gmail.com>
2023-04-04 21:15:18 +0300

    fix analytics page layout

--- ProjectManagmentApp/WebClient/project_managment_client/src/components/AnalyticsForm/AnalyticsForm.jsx
+++ ProjectManagmentApp/WebClient/project_managment_client/src/components/AnalyticsForm/AnalyticsForm.jsx
@@ -6,38 +6,43 @@ import { Pie } from 'react-chartjs-2';
 ChartJS.register(ArcElement, Tooltip, Legend);
 
 const data = {
-    labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+    labels: ['Новые', 'В работе', 'Выполненные', 'Завершённые'],
     datasets: [
         {
-            label: '# of Votes',
-            data: [12, 19, 3, 5, 2, 3],
+            label: 'кол-во задач',
+            data: [12, 19, 3, 5],
             backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
+                'rgb(235, 235, 235)',
                 'rgba(54, 162, 235, 0.2)',
                 'rgba(255, 206, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
+                'rgba(75, 192, 192, 0.2)'
             ],
             borderColor: [
-                'rgba(255, 99, 132, 1)',
+                'rgb(235, 235, 235)',
                 'rgba(54, 162, 235, 1)',
                 'rgba(255, 206, 86, 1)',
-                'rgba(75, 192, 192, 1)',
-                'rgba(153, 102, 255, 1)',
-                'rgba(255, 159, 64, 1)',
+                'rgba(75, 192, 192, 1)'
             ],
             borderWidth: 1,
         },
     ],
 };
 
+
 const AnalyticsForm = () => {
 
 
     return (
         <div className="analytics-form">
-            <Pie data={data} />;
+            <div className="analytics-form__header">
+                <p>Аналитика проекта</p>
+            </div>
+            <div className="analytics-form__form-body form-body">
+                <div className="form-body__diagram">
+                    <Pie data={data} style={{marginBottom:'20px'}}/>
+                    Статусы задач проекта
+                </div>
+            </div>
         </div>
     )
 }
