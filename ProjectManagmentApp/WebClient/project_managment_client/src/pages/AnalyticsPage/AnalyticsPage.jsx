da17b39d6f6160a2efaa9bfc729d4ac29d81543d
andrey-rsn <korowai98.ag@gmail.com>
2023-04-04 20:53:09 +0300

    create analytics page and form, add diagram

---
+++ ProjectManagmentApp/WebClient/project_managment_client/src/pages/AnalyticsPage/AnalyticsPage.jsx
@@ -0,0 +1,14 @@
+import AnalyticsForm from "../../components/AnalyticsForm/AnalyticsForm";
+import "./AnalyticsPage.css";
+
+const AnalyticsPage = () => {
+
+
+    return(
+        <div className="analytics-page">
+            <AnalyticsForm/>
+        </div>
+    )
+}
+
+export default AnalyticsPage;
